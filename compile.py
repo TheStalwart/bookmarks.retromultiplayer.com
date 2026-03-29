fb9a46db36c1f52b20ccc081b5ab94f709f3ee76
Pavel Shevchuk <stlwrt@gmail.com>
2026-03-29 05:30:29 +0300

    Reformatted compile.py with ruff

--- compile.py
+++ compile.py
@@ -1,12 +1,13 @@
 import glob
-from operator import itemgetter
 import os
 import pathlib
+from operator import itemgetter
+from urllib.parse import urlparse
+
 import yaml
+from jinja2 import Environment, FileSystemLoader
 from rich import print
 from rich.pretty import pprint
-from urllib.parse import urlparse
-from jinja2 import Environment, FileSystemLoader
 
 # Define file paths
 PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
@@ -15,24 +16,28 @@ TEMPLATE_ROOT = os.path.join(PROJECT_ROOT, "templates")
 OUTPUT_ROOT = os.path.join(PROJECT_ROOT, "output")
 
 # https://stackoverflow.com/a/1774043/5337349
-print(f'Loading [yellow]{BOOKMARKS_YAML_FILE_PATH}[/yellow]')
-with open(os.path.join(PROJECT_ROOT, BOOKMARKS_YAML_FILE_PATH)) as bookmarks_yaml_stream:
+print(f"Loading [yellow]{BOOKMARKS_YAML_FILE_PATH}[/yellow]")
+with open(
+    os.path.join(PROJECT_ROOT, BOOKMARKS_YAML_FILE_PATH)
+) as bookmarks_yaml_stream:
     file_contents = yaml.safe_load(bookmarks_yaml_stream)
     for group in file_contents["bookmarks"]:
         for bookmark in group["links"]:
-            bookmark["g_favicon_url"] = f"https://www.google.com/s2/favicons?domain={urlparse(bookmark['url']).netloc}"
+            bookmark["g_favicon_url"] = (
+                f"https://www.google.com/s2/favicons?domain={urlparse(bookmark['url']).netloc}"
+            )
     pprint(file_contents)
 
     # Load all template files
     env = Environment(loader=FileSystemLoader(TEMPLATE_ROOT))
-    for template_file_path in glob.glob(os.path.join(TEMPLATE_ROOT, '*.html')):
+    for template_file_path in glob.glob(os.path.join(TEMPLATE_ROOT, "*.html")):
         template_filename = os.path.basename(template_file_path)
         try:
-            print(f'Processing [yellow]{template_filename}[/yellow]')
+            print(f"Processing [yellow]{template_filename}[/yellow]")
             template = env.get_template(template_filename)
             output = template.render(data=file_contents)
             output_file_path = os.path.join(OUTPUT_ROOT, template_filename)
-            with open(output_file_path, 'w', newline='') as f:
+            with open(output_file_path, "w", newline="") as f:
                 f.write(output)
         except Exception as exc:
-            print(f'[red]Error processing {template_filename}: {exc}[/red]')
+            print(f"[red]Error processing {template_filename}: {exc}[/red]")
