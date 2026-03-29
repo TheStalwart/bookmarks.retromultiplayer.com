1786c643c8c09fa4ab015c12eb5e9227547e2f8a
Pavel Shevchuk <stlwrt@gmail.com>
2026-03-29 05:33:10 +0300

    Use Path instead of os.path.join(), as suggested by ruff

--- compile.py
+++ compile.py
@@ -11,15 +11,13 @@ from rich.pretty import pprint
 
 # Define file paths
 PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
-BOOKMARKS_YAML_FILE_PATH = os.path.join(PROJECT_ROOT, "bookmarks.yaml")
-TEMPLATE_ROOT = os.path.join(PROJECT_ROOT, "templates")
-OUTPUT_ROOT = os.path.join(PROJECT_ROOT, "output")
+BOOKMARKS_YAML_FILE_PATH = PROJECT_ROOT / "bookmarks.yaml"
+TEMPLATE_ROOT = PROJECT_ROOT / "templates"
+OUTPUT_ROOT = PROJECT_ROOT / "output"
 
 # https://stackoverflow.com/a/1774043/5337349
 print(f"Loading [yellow]{BOOKMARKS_YAML_FILE_PATH}[/yellow]")
-with open(
-    os.path.join(PROJECT_ROOT, BOOKMARKS_YAML_FILE_PATH)
-) as bookmarks_yaml_stream:
+with open(BOOKMARKS_YAML_FILE_PATH) as bookmarks_yaml_stream:
     file_contents = yaml.safe_load(bookmarks_yaml_stream)
     for group in file_contents["bookmarks"]:
         for bookmark in group["links"]:
@@ -29,14 +27,14 @@ with open(
     pprint(file_contents)
 
     # Load all template files
-    env = Environment(loader=FileSystemLoader(TEMPLATE_ROOT))
-    for template_file_path in glob.glob(os.path.join(TEMPLATE_ROOT, "*.html")):
+    env = Environment(loader=FileSystemLoader(str(TEMPLATE_ROOT)))
+    for template_file_path in glob.glob(str(TEMPLATE_ROOT / "*.html")):
         template_filename = os.path.basename(template_file_path)
         try:
             print(f"Processing [yellow]{template_filename}[/yellow]")
             template = env.get_template(template_filename)
             output = template.render(data=file_contents)
-            output_file_path = os.path.join(OUTPUT_ROOT, template_filename)
+            output_file_path = OUTPUT_ROOT / template_filename
             with open(output_file_path, "w", newline="") as f:
                 f.write(output)
         except Exception as exc:
